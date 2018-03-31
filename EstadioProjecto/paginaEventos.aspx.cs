16fb5836a71513b969ed0e62c96fd78ee364003b
karayaa128 <37944319+karayaa128@users.noreply.github.com>
2018-03-30 23:17:37 -0600

    Master page
    master page express probada con un web form nuevo.

--- EstadioProjecto/paginaEventos.aspx.cs
+++ EstadioProjecto/paginaEventos.aspx.cs
@@ -51,22 +51,22 @@ public partial class paginaEventos : System.Web.UI.Page
         return conn;
     }
 
-    public void innerCarouselHtml()
-    {
-        carouselControls.InnerHtml = ("<div class=" +"carousel - inner" + ">"
-      "< div class=" + "carousel-item active"+">"
-      "<div class="+"container"+">" 
-          "<img class=" + "d-block w-100" + "src=" + "http://tueventoenbogota.com/wp-content/uploads/2015/07/01_eventos_empresariales-1240x824.jpg" + "/>"
-          "<div class=" + "centered" + ">"
-              "<asp:Label ID = " + "eventNamelbl" + "runat=" + "server" Text="Nombre del evento"></asp:Label>
-          </div>
-          <div class="middle-left">
-              <asp:Label ID = "desclbl" runat="server" Text="Descripcion del evento"></asp:Label>
-          </div>
-          <div class="bottom-left">
-              <asp:Label ID = "horariolbl" runat="server" Text="Horario del evento"></asp:Label>
-          </div>
-      </div>");
-    }
+    //public void innerCarouselHtml()
+    //{
+    //carouselControls.InnerHtml = ("<div class=" +"carousel - inner" + ">"
+    // "< div class=" + "carousel-item active"+">"
+    //   "<div class="+"container"+">" 
+    //     "<img class=" + "d-block w-100" + "src=" + "http://tueventoenbogota.com/wp-content/uploads/2015/07/01_eventos_empresariales-1240x824.jpg" + "/>"
+    //   "<div class=" + "centered" + ">"
+    //     "<asp:Label ID = " + "eventNamelbl" + "runat=" + "server" Text="Nombre del evento"></asp:Label>
+    //  </div>
+    //  <div class="middle-left">
+    //     <asp:Label ID = "desclbl" runat="server" Text="Descripcion del evento"></asp:Label>
+    //  </div>
+    //  <div class="bottom-left">
+    //     <asp:Label ID = "horariolbl" runat="server" Text="Horario del evento"></asp:Label>
+    //   </div>
+    // </div>");
+    //}
 
 }
